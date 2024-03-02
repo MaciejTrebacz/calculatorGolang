24394408908c1c1a3a9fc41fa364ba8b2833a7f3
maciejtrebacz <maciekdmd90@gmail.com>
2024-03-02 20:10:13 +0100

    first

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.1, 0.2, 0.07}
+
+	result := make(map[float64][]float64) // creating dictionary with K= f64 V=f64
+}
